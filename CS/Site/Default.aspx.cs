b96479bc2f67a169cc1dbd54aeb6cc3e7d301956
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-19 15:30:46 +0400

    Change example status to out of maintenance

--- CS/Site/Default.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using DevExpress.Web;
-
-public partial class _Default : System.Web.UI.Page {
-    protected void Page_Load(object sender, EventArgs e) {
-        ASPxWebControl.RegisterBaseScript(this);
-    }
-}
